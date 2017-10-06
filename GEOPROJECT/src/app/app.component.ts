b0255b982a75dd50afaa4ec756cf1f1990b59e5b
aodren35 <aodren.letellier@etudiant.univ-rennes1.fr>
2017-10-06 10:04:05 +0200

    add leaflet

--- GEOPROJECT/src/app/app.component.ts
+++ GEOPROJECT/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+
+
+  options = {
+    layers: [
+      L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
+    ],
+    zoom: 5,
+    center: L.latLng([ 46.879966, -121.726909 ])
+  };
 }
